f995b10472e4e96ea0988b531d49d363d3bf0ccd
50084663 <50084663@est.ort.edu.ar>
2026-04-13 14:31:25 -0300

    .

---
+++ Assets/scripts 1/UIpasswordmanager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIpasswordmanager : MonoBehaviour
+{
+    public GameObject txtResultadoContra;
+    public TextMeshProUGUI txtResultadoContraTexto;
+    public TMP_InputField input_Password;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultadoContra.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void ValidatePassword()
+    {
+        txtResultadoContra.SetActive(true);
+        txtResultadoContraTexto.text = input_Password.text;
+    }
+}
+    
